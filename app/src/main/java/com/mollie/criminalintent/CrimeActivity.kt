fef282e5ba2f868eea4a2669a0d8bf84e93f125d
mollies <mstephenson@8thlight.com>
2017-08-08 10:26:19 +0100

    Converts crime activity to kotlin and configures gradle for kotlin

---
+++ app/src/main/java/com/mollie/criminalintent/CrimeActivity.kt
@@ -0,0 +1,13 @@
+package com.mollie.criminalintent
+
+import android.support.v4.app.FragmentActivity
+import android.os.Bundle
+
+
+class CrimeActivity : FragmentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crime)
+    }
+}
